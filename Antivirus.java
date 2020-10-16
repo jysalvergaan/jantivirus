55bd0e73605b2a57efbb7d85c7d4cbfe9eadb9fd
jysalvergaan <48684723+jysalvergaan@users.noreply.github.com>
2020-10-16 15:14:37 +0300

    ReleaseCommit

--- Antivirus.java
+++ Antivirus.java
@@ -3,26 +3,22 @@ package com.antivir;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 import javax.swing.*;
 import java.awt.event.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 class Antivirus {
     int count = 0;
     int size = 0;
     int occur = 0;
+    int filesChecked = 0;
     HashMap virusHashMap = new HashMap(); //HashMap for virus' keys
     String directory; //saved path
     List<String> listedFiles = new ArrayList<String>(); //List of files & directories at the path
-    List<File> subDirectories; //TODO
     List<String> infectedFiles = new ArrayList<String>(); //List of all found infected files
-    int[] depthCounter = {0, 0, 0};
-    int depthLevel = 0;
 
     static final String SIGNATURES_FILE = "source files/signatures.txt";
 
@@ -32,6 +28,9 @@ class Antivirus {
     private JButton buttonStart;
     private JTextField textFieldDir;
     private JTextArea textAreaViruses;
+    private JLabel textFieldFilesCheckedNum;
+    private JLabel labelCurrentDir;
+    private JLabel labelInfectedFilesNum;
 
     @SuppressWarnings("unchecked")
     void readSignatures(String filename) throws Exception {
@@ -53,6 +52,8 @@ class Antivirus {
 
     @SuppressWarnings("unchecked")
     void searchForVirus(String file) throws Exception {
+        occur = 0;
+        count = 0;
         FileReader in = new FileReader(file);
         BufferedReader br = new BufferedReader(in);
         String line;
@@ -67,17 +68,15 @@ class Antivirus {
                 }
             }
         }
-        System.out.println("size" + String.valueOf(size) + " | occur " + String.valueOf(occur));
+        System.out.println("size" + size + " | occur " + occur);
         br.close();
-        if (size == occur && !(size == 0 && occur == 0)) { //checking the file to contain a virus condition
-            JOptionPane.showMessageDialog(null, "Infected File", "Virus Detected ", JOptionPane.ERROR_MESSAGE);
+        if (size == occur && size != 0) { //checking the file to contain a virus condition
             System.out.println("Virus Detected");
             infectedFiles.add(file);
-            textAreaViruses.append(file); //GUI logging element
+            textAreaViruses.append(file + "\n"); //GUI logging element
         }
         else{
-            JOptionPane.showMessageDialog(null, "Clean File", "No Virus Found ", JOptionPane.INFORMATION_MESSAGE);
-            System.out.println("No Viruses Detected");
+            System.out.println("No Virus Detected");
         }
     }
 
@@ -85,8 +84,16 @@ class Antivirus {
         buttonPickDir.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                directory = textFieldDir.getText();
-                File f = new File(directory);
+                JFileChooser chooser = new JFileChooser();
+                chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                chooser.setAcceptAllFileFilterUsed(false);
+                chooser.setCurrentDirectory(new java.io.File("."));
+                chooser.setDialogTitle("Select directory:");
+
+                if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    directory = chooser.getSelectedFile().toString();
+                }
+                labelCurrentDir.setText("Searching in: " + directory);
             }
         });
 
@@ -94,21 +101,53 @@ class Antivirus {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
+                    buttonStart.setEnabled(false);
                     readSignatures(SIGNATURES_FILE);
 
-                    try (Stream<Path> filesTree = Files.walk(Paths.get(directory), 10
+                    try {
+                        Files.walkFileTree(Paths.get(directory), new HashSet<>(), 10, new FileVisitor<Path>() {
+                            @Override
+                            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                                return FileVisitResult.CONTINUE;
+                            }
 
+                            @Override
+                            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                                //System.out.println("Visited file: " + file);
+                                if(Files.size(file) < 5000000) {
+                                    try {
+                                        searchForVirus(file.toString());
+                                    } catch (Exception exception) {
+                                        exception.printStackTrace();
+                                    }
+                                    filesChecked++;
+                                    textFieldFilesCheckedNum.setText("Files checked: " + filesChecked);
+                                    textFieldFilesCheckedNum.setVerticalTextPosition(SwingConstants.CENTER);
+                                    listedFiles.add(file.toString());
+                                    return FileVisitResult.CONTINUE;
+                                }
+                                else
+                                    return FileVisitResult.CONTINUE;
+                            }
 
-                            )) {
-                        listedFiles = filesTree.map(path -> Files.isRegularFile(path) ? path.toString() + '/' : path.toString()).collect(Collectors.toList());
+                            @Override
+                            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                                return FileVisitResult.SKIP_SUBTREE;
+                            }
+
+                            @Override
+                            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                                return FileVisitResult.CONTINUE;
+                            }
+                        });
                     }
                     catch (Exception exc) {
                         System.out.println("Tree walking error: " + exc);
                     }
+                    labelInfectedFilesNum.setText("Infected files found: " + infectedFiles.size());
+                    buttonStart.setEnabled(true);
 
-                    for (String f: listedFiles) {
-                        searchForVirus(f);
-                    }
+
 
                 } catch(Exception err) {
                     System.out.println("Startup Error: " + err);
@@ -119,13 +158,13 @@ class Antivirus {
 
 
     public static void main(String[] args) {
-
         Antivirus av = new Antivirus();
 
-        JFrame mainFrame = new JFrame("Main");
+        JFrame mainFrame = new JFrame("Simple Antivirus");
         mainFrame.setContentPane(av.formMain);
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         mainFrame.pack();
+        mainFrame.setResizable(false);
         mainFrame.setVisible(true);
 
     }
